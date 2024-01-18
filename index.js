c1253d42b1ba5ac93be2fd5753e3b585095f4c5c
zeinab <swarsama@hotmail.co.uk>
2024-01-18 22:40:44 +0000

    adding event listener click to button and obtaining innerText

---
+++ index.js
@@ -0,0 +1,27 @@
+// console.log("hello");
+
+// const ratingBtns = document.querySelectorAll(".btn");
+
+// ratingBtns.forEach((btn) => {
+//   // console.log(btn);
+//   btn.addEventListener("click", (event) => {
+//     console.log("Clicked the button!");
+
+//   })
+// });
+
+document.querySelectorAll(".btn").forEach((btn) => {
+  // console.log(btn);
+  btn.addEventListener("click", (event) => {
+    // console.log("Clicked the button!");
+    // use event.target.innerText to indicate rating
+    console.log(event.target.innerText);
+  });
+});
+
+
+// document.querySelectorAll("img").forEach((img) => {
+//   img.addEventListener("click", (event) => {
+//     event.currentTarget.classList.toggle("img-circle");
+//   });
+// });
